f2d5c849f0e8385672b706ab2969d954d11f5690
Harold Gautschi <telemac.dev@gmail.com>
2023-05-26 13:32:37 -0400

    initial

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('tag/', include('mptt_tags.urls')), # url do app
+    path('select/', include('select2.urls')), # url do app
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Adicionar Isto
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Adicionar Isto
---
+++ mptt_tag/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MpttTagConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mptt_tag'
